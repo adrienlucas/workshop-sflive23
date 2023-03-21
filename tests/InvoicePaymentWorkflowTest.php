fc78acc0091eff79401018b2492fb981bc9771a7
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2023-03-21 10:50:48 +0100

    Full InvoicePaymentWorkflowTest

--- tests/InvoicePaymentWorkflowTest.php
+++ tests/InvoicePaymentWorkflowTest.php
@@ -10,7 +10,7 @@ class InvoicePaymentWorkflowTest extends KernelTestCase
 {
     public function testPaymentWorkflowTransitions(): void
     {
-        $kernel = self::bootKernel();
+        self::bootKernel();
         // debug:container payment
         // > WorkflowInterface
         $paymentWorkflow = static::getContainer()->get('state_machine.invoice_payment');
@@ -21,5 +21,27 @@ class InvoicePaymentWorkflowTest extends KernelTestCase
 
         $paymentWorkflow->getMarking($invoice);
         $this->assertSame('required', $invoice->getPaymentState());
+
+        $this->assertFalse($paymentWorkflow->can($invoice, 'pay'));
+        $this->assertFalse($paymentWorkflow->can($invoice, 'fail'));
+
+        $this->assertTrue($paymentWorkflow->can($invoice, 'submit_request'));
+        $this->assertTrue($paymentWorkflow->can($invoice, 'abort'));
+
+        $paymentWorkflow->apply($invoice, 'submit_request');
+        $this->assertSame('pending', $invoice->getPaymentState());
+
+        $this->assertFalse($paymentWorkflow->can($invoice, 'submit_request'));
+        $this->assertFalse($paymentWorkflow->can($invoice, 'abort'));
+
+        $this->assertTrue($paymentWorkflow->can($invoice, 'pay'));
+        $this->assertTrue($paymentWorkflow->can($invoice, 'fail'));
+
+        $paymentWorkflow->apply($invoice, 'pay');
+        $this->assertSame('paid', $invoice->getPaymentState());
+        $this->assertFalse($paymentWorkflow->can($invoice, 'submit_request'));
+        $this->assertFalse($paymentWorkflow->can($invoice, 'abort'));
+        $this->assertFalse($paymentWorkflow->can($invoice, 'pay'));
+        $this->assertFalse($paymentWorkflow->can($invoice, 'fail'));
     }
 }
